3d8a5d9320dd8a54ee2deb05fa49b344d7a0d304
Scott Benson <61036304+sbenson189@users.noreply.github.com>
2021-05-10 10:11:42 -0500

    Changed ContentView to LoginView

--- TechnologyQuizApp/TechnologyQuizApp/TechnologyQuizAppApp.swift
+++ TechnologyQuizApp/TechnologyQuizApp/TechnologyQuizAppApp.swift
@@ -13,7 +13,7 @@ struct TechnologyQuizAppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
