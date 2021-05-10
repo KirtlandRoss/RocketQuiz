354f810e285848c19a13acf8ef49555f293cd9f0
Kirtland Ross <kirtlandross@gmail.com>
2021-05-10 17:17:55 -0400

    Shrank login image, wrapped everything in a  NavigationView

--- TechnologyQuizApp/TechnologyQuizApp/Views (UI)/LoginPageView.swift
+++ TechnologyQuizApp/TechnologyQuizApp/Views (UI)/LoginPageView.swift
@@ -18,97 +18,106 @@ struct LoginView: View {
     }
     
     var body: some View {
-        ZStack {
-            Color.purpleGray
-                .ignoresSafeArea()
-            
-            VStack {
-              Text("Quiz App")
-                .font(.system(size: 60))
-                .fontWeight(.bold)
-                .foregroundColor(Color.white)
-
-                Image("rocket")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle())
-                    .overlay(Circle()
-                        .stroke(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ), lineWidth: 4))
-                    .shadow(radius: 10)
-                    .padding(.bottom, 50)
-                
+        NavigationView {
+            ZStack {
+                Color.purpleGray
+                    .ignoresSafeArea()
                 VStack {
-                    CustomTextField(
-                        placeholder: Text("Username").foregroundColor(.gray),
-                        text: $username
-                    )
-                      .padding()
-                      .foregroundColor(.white)
-                      .background(Color.lightPurpleGray)
-                      .cornerRadius(20.0)
-                                  
-                    CustomTextField(
-                        placeholder: Text("Password").foregroundColor(.gray),
-                        text: $password
-                    )
-                      .padding()
-                      .foregroundColor(.white)
-                      .background(Color.lightPurpleGray)
-                      .cornerRadius(20.0)
-                    
-                    Toggle("Remember Me", isOn: $rememberMe)
-                        .toggleStyle(SwitchToggleStyle(tint: .purple))
+                    Text("Quiz App")
+                        .font(.system(size: 60))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+
+                    Image("rocket")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .overlay(Circle()
+                                    .stroke(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ), lineWidth: 4))
+                        .shadow(radius: 10)
+                        .padding(.bottom, 50)
+
+                    VStack {
+                        CustomTextField(
+                            placeholder: Text("Username").foregroundColor(.gray),
+                            text: $username
+                        )
+                        .padding()
                         .foregroundColor(.white)
-                        .offset(x: 0, y: 5)
-                        .padding([.leading, .trailing], 5)
+                        .background(Color.lightPurpleGray)
+                        .cornerRadius(20.0)
+
+                        CustomTextField(
+                            placeholder: Text("Password").foregroundColor(.gray),
+                            text: $password
+                        )
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.lightPurpleGray)
+                        .cornerRadius(20.0)
+
+                        Toggle("Remember Me", isOn: $rememberMe)
+                            .toggleStyle(SwitchToggleStyle(tint: .purple))
+                            .foregroundColor(.white)
+                            .offset(x: 0, y: 5)
+                            .padding([.leading, .trailing], 5)
                         if rememberMe {
                         }
-                }.padding([.leading, .trailing], 27.5)
-                
-                Button(action: { action() }) {
-                  AppImage(width: 50, height: 50, cornerRadius: 0, name: "facebook")
-                }
-                
-                Button(action: { action() }) {
-                  Text("Sign In")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 300, height: 50)
-                    .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
-                    .cornerRadius(15.0)
-                }.offset(x: 0, y: 50)
-                
-                Button(action: { action() }) {
-                  Text("Forgot Password")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 300, height: 50)
-                    .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
-                    .cornerRadius(15.0)
-                }.offset(x: 0, y: 50)
-                
-                Text("Dont have an account?")
-                    .font(.system(size: 20))
-                    .foregroundColor(.white)
-                    .padding()
-                    .cornerRadius(15.0)
-                    .offset(x: 0, y: 70)
-                
-                Button(action: { action() }) {
-                  Text("Tap here to sign up!")
-                    .font(.system(size: 30))
-                    .fontWeight(.bold)
-                    .foregroundColor(.purple)
-                    .padding()
-                    .cornerRadius(15.0)
-                }.offset(x: 0, y: 30)
+                    }.padding([.leading, .trailing], 27.5)
+
+                    Button(action: { action() }) {
+                        AppImage(width: 50, height: 50, cornerRadius: 0, name: "facebook")
+                    }
+
+                    Button(action: { action() }) {
+                        Text("Sign In")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
+                            .cornerRadius(15.0)
+                    }.offset(x: 0, y: 50)
+
+                    Button(action: { action() }) {
+                        Text("Forgot Password")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
+                            .cornerRadius(15.0)
+                    }.offset(x: 0, y: 50)
+
+                    Text("Dont have an account?")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .padding()
+                        .cornerRadius(15.0)
+                        .offset(x: 0, y: 70)
+
+
+
+                    NavigationLink(destination: SignUpView()){
+                        Text("Tap here to sign up!")
+                            .font(.system(size: 30))
+                            .fontWeight(.bold)
+                            .foregroundColor(.purple)
+                            .padding()
+                            .cornerRadius(15.0)
+
+                    }.offset(x: 0, y: 30)
+
+                    }
+
             }
         }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
     }
+
 }
 
 struct LoginView_Previews: PreviewProvider {
