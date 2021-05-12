a960fd69be0e6abebdf32c0ddbfff74d3c8cd7f7
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-12 10:47:34 -0400

    - Began adding methods to QuestionBank+CoreDataClass - Comments include personal train of thought for easy memory and communication

--- TechnologyQuizApp/QuestionBank+CoreDataClass.swift
+++ TechnologyQuizApp/QuestionBank+CoreDataClass.swift
@@ -10,6 +10,35 @@ import Foundation
 import CoreData
 
 
-public class QuestionBank: NSManagedObject {
-
+public class QuestionBank: NSManagedObject
+{
+    /*
+     Jared's to-do list:
+     - make method(s) to get questions and store in data structure
+     - randomization
+     
+     we only want 25 questions in the array per gameplay session. yes there will be more than 25 total in coredata
+     
+     Kirt said there should be a fetch method in some extension
+     */
+    
+    // Store question objects here
+    static var questions = [Question]()
+    
+    /*func getQs()
+    {
+        var tempQ = [Question]()
+        let fetchReq = NSFetchRequest<NSFetchRequestResult>(entityName: "Question")
+        
+        do
+        {
+            tempQ = try DBHelper.inst.context
+        }
+        catch
+        {
+            
+        }
+    }*/
+    
+    
 }
