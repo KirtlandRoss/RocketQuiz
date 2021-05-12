77cf92760eb081710057ecb7020820cdea6c5f8d
Scott Benson <61036304+sbenson189@users.noreply.github.com>
2021-05-12 16:34:55 -0500

    Updated Admin Screen UI

--- TechnologyQuizApp/TechnologyQuizApp/Views (UI)/AdminView.swift
+++ TechnologyQuizApp/TechnologyQuizApp/Views (UI)/AdminView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AdminView: View {
+    @State var blockUsername: String = ""
     func action() {
         
     }
@@ -21,20 +22,93 @@ struct AdminView: View {
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
                 .padding(.bottom, 50)
-            HStack {
+                .position(x: 220, y: 100)
+            
+            VStack {
+                Text("Create Quiz")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
                 Button(action: { action() }) {
-                  Text("Submit")
+                  Text("Create Quiz")
                     .font(.headline)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .padding()
-                    .frame(width: 125, height: 100)
+                    .frame(width: 150, height: 50)
                     .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
                     .cornerRadius(15.0)
-                }.offset(x: 0, y: 100)
+                }.frame(width: 200, height: 100, alignment: .center)
             }
+            .padding(20)
+            .border(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing), width: 4)
+            .cornerRadius(10)
+            .offset(y: -180)
+            
+            // View User Scores
+            VStack {
+                Text("Get User Scores")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Button(action: { action() }) {
+                  Text("Get Scores")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
+                    .cornerRadius(15.0)
+                }.frame(width: 200, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            .padding(20)
+            .border(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing), width: 4)
+            .cornerRadius(10)
+            
+            // Block User
+            VStack {
+                Text("Block User")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                CustomTextField(
+                    placeholder: Text("Username to block").foregroundColor(.gray),
+                    text: $blockUsername
+                )
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.lightPurpleGray)
+                .cornerRadius(20.0)
+                .frame(width: 200, height: 100, alignment: .leading)
+                Button(action: { action() }) {
+                  Text("Block")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
+                    .cornerRadius(15.0)
+                }
+            }
+            .padding(20)
+            .border(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ), width: 4)
+            .cornerRadius(10)
+            .offset(y: 210)
+            
+            // Sign Out Button
+            Button(action: { action() }) {
+              Text("Sign Out")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 250, height: 70)
+                .background(LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading , endPoint: .bottomTrailing ))
+                .cornerRadius(15.0)
+            }.offset(x: 0, y: 390)
         }
-        
     }
 }
 
