3ac75f6d0d96fcac14cc98bfbbd3bef706683972
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-12 12:08:00 -0400

    Question getting - Added getQs() method to QuestionBank+CoreDataClass - Bank can now query core data for questions and pick a batch of 25 to use in game - Testing requested

--- TechnologyQuizApp/QuestionBank+CoreDataClass.swift
+++ TechnologyQuizApp/QuestionBank+CoreDataClass.swift
@@ -15,30 +15,64 @@ public class QuestionBank: NSManagedObject
     /*
      Jared's to-do list:
      - make method(s) to get questions and store in data structure
+        PROGRESS SO FAR
      - randomization
      
      we only want 25 questions in the array per gameplay session. yes there will be more than 25 total in coredata
      
-     Kirt said there should be a fetch method in some extension
+     Kirt said there should be a fetch method in property file extensions (Apres fait: C'est vrai)
+     Don't mess w/ property files and settings
      */
     
-    // Store question objects here
-    static var questions = [Question]()
-    
-    /*func getQs()
+    func getQs()
     {
-        var tempQ = [Question]()
+        // get array of all questions and then call extension methods to add to bank
+        var quest = [Question]()
         let fetchReq = NSFetchRequest<NSFetchRequestResult>(entityName: "Question")
+        
+        // create context for query
+        let context = NSManagedObjectContext()
         
         do
         {
-            tempQ = try DBHelper.inst.context
+            quest = try context.fetch(fetchReq) as! [Question]
         }
         catch
         {
-            
+            print("fetch request failed in method getQs :( error: \(error)")
+            return
         }
-    }*/
+        
+        // If we don't have questions it doesn't make sense to stay here
+        if quest.isEmpty
+        {
+            return
+        }
+        
+        // ppreventing index out of bounds
+        if quest.count < 25
+        {
+            print("Less than 25 questions detected, please add more!")
+            return
+        }
+        
+        // picking 25 questions
+        for _ in 0..<25
+        {
+            let indexCap = quest.count
+                
+            // random index get
+            let indexUse = Int.random(in: 0..<indexCap)
+                
+            // Add to the bank
+            self.addToQuestions(quest[indexUse])
+                
+            // don't want repeats
+            quest.remove(at: indexUse)
+        }
+        
+        try! context.save()
+    }
     
     
 }
